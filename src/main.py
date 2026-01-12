0c09ca959148558a5f447ee7195622167821218d
Juan Carlos Martínez Moreno <57271164+juankaUAB@users.noreply.github.com>
2026-01-12 18:30:32 +0100

    Updated workflow

--- src/main.py
+++ src/main.py
@@ -1,29 +1,37 @@
-from flask import Flask
-import requests
 from datetime import datetime, timezone
 import itertools
+from flask import Flask
+import requests
 
 APP_VERSION = "v0.0.2"
-senseBox_list = ["5eba5fbad46fb8001b799786","5c21ff8f919bf8001adf2488","5ade1acf223bd80019a1011c"]
+SENSEBOX_LIST = ["5eba5fbad46fb8001b799786","5c21ff8f919bf8001adf2488","5ade1acf223bd80019a1011c"]
 
 app = Flask(__name__)
 
 @app.route('/')
 def index() -> dict[str]:
+    """Entrypoint for the App"""
     return {"body" : "Welcome to HiveBox!"}
 
 @app.route('/version')
 def version() -> dict[str, str]:
+    """Version endpoint"""
     return {"api" : "version", "body" : APP_VERSION}
 
 @app.route('/temperature')
 def temperature():
+    """Temperature endpoint"""
     temperature_list = []
-    for senseBox_id in senseBox_list:
-        response = requests.get("https://api.opensensemap.org/boxes/" + senseBox_id, timeout=30).json()
-        temperature_list.append(list(filter(lambda x: (x["title"]=="Temperatur" or x["title"]=="temperature") and (datetime.strptime(response["sensors"][2]["lastMeasurement"]["createdAt"], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc).timestamp() > datetime.now().timestamp() - 3600),response["sensors"])))
-    temperature_list = list(map(lambda x: float(x["lastMeasurement"]["value"]), list(itertools.chain.from_iterable(temperature_list))))
-    return {"senseBox_ids" : senseBox_list, "type" : "avg", "measurement" : "Temperature", "value" : sum(temperature_list) / len(temperature_list)}
+    for senseBox_id in SENSEBOX_LIST:
+        response = requests.get("https://api.opensensemap.org/boxes/" + senseBox_id,
+                                timeout=30).json()
+        temperature_list.append(list(filter(lambda x:
+            (x["title"]=="Temperatur" or x["title"]=="temperature") and
+            (datetime.strptime(response["sensors"][2]["lastMeasurement"]["createdAt"], '%Y-%m-%dT%H:%M:%S.%fZ')
+             .replace(tzinfo=timezone.utc).timestamp() > datetime.now().timestamp() - 3600),response["sensors"])))
+    temperature_list = list(map(lambda x: float(x["lastMeasurement"]["value"]),
+                                list(itertools.chain.from_iterable(temperature_list))))
+    return {"senseBox_ids" : SENSEBOX_LIST, "type" : "avg", "measurement" : "Temperature", "value" : sum(temperature_list) / len(temperature_list)}
 
 if __name__ ==  '__main__':
       app.run(host="0.0.0.0", port=8000)
